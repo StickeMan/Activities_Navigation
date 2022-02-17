9fc304f3b86ec07f28bfb253eb4f7ff10f579faf
StickeMan <2005040@utrivieramaya.edu.mx>
2022-02-16 23:54:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/activities_navigation/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.activities_navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button boton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boton = (Button) findViewById(R.id.button);
+        boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                Intent intent = new Intent(this,MainActivity2.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
